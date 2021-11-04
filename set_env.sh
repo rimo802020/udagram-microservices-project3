80675aa372169953534395f7d499d9029585ff0d
rimo802020 <reem.elmorsy@gmail.com>
2021-11-04 21:40:17 +0200

    Initial Commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=rimodbudacity
+export POSTGRES_PASSWORD=rimodbudacity
+export POSTGRES_HOST=rimodbudacity.cnomm0vspacj.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::udacity-project3-rimo
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
